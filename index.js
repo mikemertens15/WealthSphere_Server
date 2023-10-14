fd3b396ab5e22b5357162c93983cd18ed204bb18
mikemertens15 <mikemertens15@gmail.com>
2023-10-14 13:04:31 -0400

    removed jwt import

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const User = require("./models/user_model");
 const {
   Configuration,
