9fb286ff2fb26d7515f3c8772e280c0c604ca8f2
mikemertens15 <mikemertens15@gmail.com>
2023-12-11 22:59:54 -0500

    Refactoring function layout

--- src/controllers/financialDataController.js
+++ src/controllers/financialDataController.js
@@ -8,53 +8,10 @@
 
 // When an account (item) gets linked, update stats
 const User = require("../models/user_model");
+const Account = require("../models/account_model");
 const PlaidItem = require("../models/plaid_item_model");
 const Transaction = require("../models/transaction_model");
 const mongoose = require("mongoose");
-
-exports.dashboardData = async (req, res) => {
-  // for current dashboard, need net worth, recent transactions (~5-6), data for spending chart
-  // TODO: test multiple accounts
-  const email = req.query.email;
-  try {
-    const user = await User.findOne({
-      email: email,
-    });
-
-    if (!user) {
-      return res.status(404).json({ status: "error", error: "User not found" });
-    }
-
-    const netWorth = user.financialStats.netWorth;
-
-    // TODO: If merchant name is null, pass in some default value
-
-    // Rewrite to account for new schema
-    const recentTransactions = await Transaction.aggregate([
-      {
-        $match: {
-          user: user._id,
-        },
-      },
-      {
-        $sort: {
-          date: -1,
-        },
-      },
-      {
-        $limit: 10,
-      },
-    ]);
-
-    res.json({
-      netWorth: netWorth,
-      recentTransactions: recentTransactions,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ status: "Error", error: err });
-  }
-};
 
 exports.netWorth = async (req, res) => {
   // return a $ figure to front-end, maybe with detailed breakdown of accounts?
