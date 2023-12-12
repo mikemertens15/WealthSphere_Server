de9d11894302e41cf5ed00e05b634516cba4da49
mikemertens15 <mikemertens15@gmail.com>
2023-12-11 22:59:38 -0500

    Work on balance route

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,17 @@ const {
 } = require("./controllers/plaidController");
 const { register, login } = require("./controllers/authController");
 const {
-  dashboardData,
   createBudget,
   addManualTransaction,
   getTransactions,
   getAccounts,
 } = require("./controllers/financialDataController");
+const {
+  getDashboardData,
+} = require("./controllers/pageData/dashBoardPageData");
+const {
+  getBalancePageData,
+} = require("./controllers/pageData/balancesPageData");
 const { deleteUserPlaidItems } = require("./controllers/deleteUserPlaidItems");
 
 // Auth Routes
@@ -23,11 +28,14 @@ router.get("/api/create_link_token", createLinkToken);
 router.post("/api/exchange_public_token", exchangePublicToken);
 
 // Finance Data routes
-router.get("/api/get_dashboard_data", dashboardData);
 router.get("/api/get_transactions", getTransactions);
 router.get("/api/get_accounts", getAccounts);
 router.post("/api/create_budget", createBudget);
 router.post("/api/add_manual_transaction", addManualTransaction);
+
+// Page Data Routes
+router.get("/api/get_dashboard_data", getDashboardData);
+router.get("/api/get_balance_page_data", getBalancePageData);
 
 // God Mode
 // Delete all plaid items for a user, used for testing and cleanup
