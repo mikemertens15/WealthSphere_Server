5608006cf03083091badaacfa58cde96f164c8eb
mikemertens15 <mikemertens15@gmail.com>
2023-12-16 15:38:41 -0500

    work on new pages

---
+++ src/controllers/pageData/accountsPageData.js
@@ -0,0 +1,15 @@
+const getAccounts =
+  require("../../controllers/financialDataController").getAccounts;
+
+// Get accounts
+exports.getAccountsPageData = async (req, res) => {
+  try {
+    const accounts = await getAccounts(req.query.email);
+    res.json({
+      accounts: accounts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "Error", error: err });
+  }
+};
---
+++ src/controllers/pageData/billsPageData.js
@@ -0,0 +1,3 @@
+const User = require("../../models/user_model");
+
+exports.getBillsPageData = async (req, res) => {};
