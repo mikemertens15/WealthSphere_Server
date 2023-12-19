caf878a687c97d1daeb447edf5b63f0722c71545
mikemertens15 <mikemertens15@gmail.com>
2023-12-18 22:35:38 -0500

    added a route for getting a user's bills

--- src/controllers/pageData/billsPageData.js
+++ src/controllers/pageData/billsPageData.js
@@ -1,3 +1,10 @@
 const User = require("../../models/user_model");
 
-exports.getBillsPageData = async (req, res) => {};
+exports.getBillsPageData = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("bills");
+    res.status(200).json({ bills: user.bills });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
